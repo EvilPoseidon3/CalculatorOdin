7aecfa2e5eaacaa56aeea1d86725a26aa4886648
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-26 14:24:38 -0500

    basic functions for adding number to display

--- script.js
+++ script.js
@@ -19,8 +19,14 @@ let equal = document.querySelector('#equalButton');
 
 let historyDisplay = document.querySelector('#historyDisplay');
 
-one.addEventListener('click', (event) => {});
-two.addEventListener('click', (event) => {});
+one.addEventListener('click', (event) => {
+    addNumDis(1)
+});
+
+two.addEventListener('click', (event) => {
+    addNumDis(2)
+});
+
 three.addEventListener('click', (event) => {});
 four.addEventListener('click', (event) => {});
 five.addEventListener('click', (event) => {});
@@ -29,11 +35,36 @@ seven.addEventListener('click', (event) => {});
 eight.addEventListener('click', (event) => {});
 nine.addEventListener('click', (event) => {});
 
-plus.addEventListener('click', (event) => {});
+plus.addEventListener('click', (event) => {
+    addOppDis("+")
+});
+
 minus.addEventListener('click', (event) => {});
 divid.addEventListener('click', (event) => {});
 multiply.addEventListener('click', (event) => {});
 equal.addEventListener('click', (event) => {});
 
 clear.addEventListener('click', (event) => {});
+
+// function for adding a number to the display array
+let displayArr = [];
+    function addNumDis(x) {
+        displayArr.push(x);
+        ammendDisplay();
+
+    };
+
+// function to add display array to the display
+    function ammendDisplay() {
+        let arrString = displayArr.toString();
+        let commaDel = arrString.replace(/,/g, "");
+        display.textContent = commaDel;
+    };
+
+
+    function addOppDis(x) {
+        displayArr.push(" ",x, " ");
+        ammendDisplay();
+    
+    };
 
