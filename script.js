424e7691c171a94572f1551c129485c0f17ae028
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-26 13:23:07 -0500

    queryselectors and event listener intial

---
+++ script.js
@@ -0,0 +1,39 @@
+let clear = document.querySelector('#clearButton');
+let display = document.querySelector('#display');
+
+let one = document.querySelector('#numberOne');
+let two = document.querySelector('#numberTwo');
+let three = document.querySelector('#numberThree');
+let four = document.querySelector('#numberFour');
+let five = document.querySelector('#numberFive');
+let six = document.querySelector('#numberSix');
+let seven = document.querySelector('#numberSeven');
+let eight = document.querySelector('#numberEight');
+let nine = document.querySelector('#numberNine');
+
+let plus = document.querySelector('#plusButton');
+let minus = document.querySelector('#minusButton');
+let divid = document.querySelector('#dividButton');
+let multiply = document.querySelector('#multiplyButton');
+let equal = document.querySelector('#equalButton');
+
+let historyDisplay = document.querySelector('#historyDisplay');
+
+one.addEventListener('click', (event) => {});
+two.addEventListener('click', (event) => {});
+three.addEventListener('click', (event) => {});
+four.addEventListener('click', (event) => {});
+five.addEventListener('click', (event) => {});
+six.addEventListener('click', (event) => {});
+seven.addEventListener('click', (event) => {});
+eight.addEventListener('click', (event) => {});
+nine.addEventListener('click', (event) => {});
+
+plus.addEventListener('click', (event) => {});
+minus.addEventListener('click', (event) => {});
+divid.addEventListener('click', (event) => {});
+multiply.addEventListener('click', (event) => {});
+equal.addEventListener('click', (event) => {});
+
+clear.addEventListener('click', (event) => {});
+
